5a42a00f2b3aeeea405b65a22a8f69f88da71475
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-03-09 00:59:03 -0800

    fixBug: import CustomersDisplay from components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import CustomersDisplay from './pages/CustomersDisplay';
+import CustomersDisplay from './components/CustomersDisplay';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
